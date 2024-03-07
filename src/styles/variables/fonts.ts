cc3076f91db8c5cb8c18ba7dd62cdbc2a7dbba33
donghhan <apensia914@gmail.com>
2024-03-07 10:39:14 +0700

    sass & variables configuration

---
+++ src/styles/variables/fonts.ts
@@ -0,0 +1,16 @@
+import { Inter, Noto_Sans_KR, Noto_Sans_HK } from "next/font/google";
+
+export const inter = Inter({
+  subsets: ["latin"],
+  weight: ["200", "300", "400", "500", "600"],
+});
+
+export const notoSansKR = Noto_Sans_KR({
+  subsets: ["latin"],
+  weight: ["200", "300", "400", "500", "600"],
+});
+
+export const notoSansHK = Noto_Sans_HK({
+  subsets: ["latin"],
+  weight: ["200", "300", "400", "500", "600"],
+});
