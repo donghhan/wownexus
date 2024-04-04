0907ddf5a3be90ebab825de5af9f19095107fc0b
donghhan <apensia914@gmail.com>
2024-04-04 08:55:17 +0700

    Auth menu added

---
+++ src/components/AuthHeader.tsx
@@ -0,0 +1,26 @@
+import Image from "next/image";
+import Link from "next/link";
+
+export default function AuthHeader() {
+  return (
+    <nav
+      className="flex items-center justify-end h-20 lg:fixed lg:right-10 lg:top-7 lg:h-fit"
+      style={{ border: "1px solid red;" }}
+    >
+      <Link
+        href="/auth"
+        id="auth"
+        className="flex items-center gap-x-1 cursor-pointer"
+      >
+        <Image
+          src="/icon/auth_profile.svg"
+          alt="anonymous profile icon"
+          width={40}
+          height={40}
+          quality={100}
+          priority={true}
+        />
+      </Link>
+    </nav>
+  );
+}
--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -2,7 +2,11 @@
 import { useFormStatus } from "react-dom";
 import { useTranslations } from "next-intl";
 
-export default function Button() {
+interface ButtonProp {
+  text: string;
+}
+
+export default function Button({ text }: ButtonProp) {
   const { pending } = useFormStatus();
   const t = useTranslations("Button");
 
@@ -11,7 +15,7 @@ export default function Button() {
       className={`w-max border-slate-50 bg-zinc-600 text-slate-400 px-14 py-3 rounded-md cursor-pointer hover:bg-zinc-500 active:bg-zinc-600 duration-300 disabled:bg-zinc-700 disabled:cursor-not-allowed`}
       disabled={pending}
     >
-      {pending ? t("loading") : t("submit")}
+      {pending ? t("loading") : text}
     </button>
   );
 }
